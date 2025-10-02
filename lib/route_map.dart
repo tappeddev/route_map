8300e6fa85871fd1dcce967cd3210511de2d9c16
Stefan Schaller <stefan.schaller@tapped.dev>
2025-10-02 09:11:57 +0200

    export service layer

--- lib/route_map.dart
+++ lib/route_map.dart
@@ -14,4 +14,5 @@ export 'package:maplibre_gl/maplibre_gl.dart'
 export 'src/model/route_map_icon/route_map_icon.dart';
 export 'src/model/route_map_icon_anchor.dart';
 export 'src/model/route_map_route/route_map_route.dart';
+export 'src/model/no_service_area_layer.dart';
 export 'src/route_map_base.dart';
